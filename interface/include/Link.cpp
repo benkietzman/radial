d173908338ef3f9cc8c699c21f2fd072b58ddd9e
Ben Kietzman <ben@kietzman.org>
2022-08-09 14:27:18 -0500

    Saved work.

--- interface/include/Link.cpp
+++ interface/include/Link.cpp
@@ -441,7 +441,7 @@ void Link::process(string strPrefix)
               {
                 link->bRetry = false;
                 ssMessage.str("");
-                ssMessage << strPrefix << "->Utility::sslConnect() [" << link->strNode << "," << link->fdSocket << "]:  Connected link.";
+                ssMessage << strPrefix << "->Utility::sslConnect() [" << link->strServer << "," << link->fdSocket << "]:  Connected link.";
                 log(ssMessage.str());
               }
               else
