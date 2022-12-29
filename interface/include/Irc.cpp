38e5fa6701dd366ae19a94883ade30dc9e02f6f5
Ben Kietzman <ben@kietzman.org>
2022-12-29 13:05:28 -0600

    Saved work.

--- interface/include/Irc.cpp
+++ interface/include/Irc.cpp
@@ -583,6 +583,9 @@ void Irc::bot(string strPrefix)
           delete[] fds;
           if (shutdown() || !isMasterSettled() || !isMaster())
           {
+            ssMessage.str("");
+            ssMessage << strPrefix << ":  Quitting IRC.";
+            log(ssMessage.str());
             quit();
           }
           // }}}
