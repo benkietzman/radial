7d2fe2a58b036a4c37efbda6875269cf2caf8fe6
Ben Kietzman <ben@kietzman.org>
2023-03-16 13:18:57 -0500

    Saved work.

--- interface/include/Interface.cpp
+++ interface/include/Interface.cpp
@@ -757,7 +757,11 @@ void Interface::process(string strPrefix)
       fds[unIndex].events = POLLOUT;
       unIndex++;
     }
-    if ((nReturn = poll(fds, unIndex, 500)) > 0)
+    if (fds[0].fd != 0 || (fds[1] != -1 && fds[1] != 1))
+    {
+      bExit = true;
+    }
+    if (!bExit && (nReturn = poll(fds, unIndex, 500)) > 0)
     {
       if (fds[0].revents & (POLLHUP | POLLIN))
       {
@@ -907,7 +911,7 @@ void Interface::process(string strPrefix)
         }
       }
     }
-    else if (nReturn < 0 && errno != EINTR)
+    else if (!bExit && nReturn < 0 && errno != EINTR)
     {
       bExit = true;
       ssMessage.str("");
