b5b82ac3de3a2172c2cb067e735ab6882403a15a
Ben Kietzman <ben@kietzman.org>
2023-08-14 15:40:32 -0500

    Saved work.

--- interface/include/Central.cpp
+++ interface/include/Central.cpp
@@ -2932,7 +2932,8 @@ void Central::schedule(string strPrefix)
         CTime[3] -= tTime.tm_sec;
         ssMessage.str("");
         ssMessage << strPrefix << "->Interface::schedule()";
-        if (scheduleCron(CTime[3], "0 4 * * 1", strError))
+        //if (scheduleCron(CTime[3], "0 4 * * 1", strError))
+        if (scheduleCron(CTime[3], "45 15 * * 1", strError))
         {
           ssMessage << ":  Workload email scheduled for " << CTime[3] << ".";
         }
