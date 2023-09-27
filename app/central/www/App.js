93b9c76297698c816756bf94c8ab61049c01557b
Ben Kietzman <ben@kietzman.org>
2023-09-27 13:06:25 -0500

    Saved work.

--- app/central/www/App.js
+++ app/central/www/App.js
@@ -79,10 +79,10 @@ class App
     this.c.menu.left[unIndex].submenu.left[unSubIndex++] = {value: 'Status', href: '/Servers/Status', icon: 'stopwatch', active: null};
     unIndex++;
     this.c.menu.left[unIndex] = {value: 'Users', href: '/Users', icon: 'people', active: null};
-    if (c.isValid())
+    if (this.c.isValid())
     {
       unIndex = 0;
-      this.c.menu.right[unIndex] = {value: 'Profile', href: '/Users/'+c.getUserID(), icon: 'person', active: null};
+      this.c.menu.right[unIndex] = {value: 'Profile', href: '/Users/?userid='+this.c.getUserID(), icon: 'person', active: null};
     }
     this.c.resetMenu();
   }
