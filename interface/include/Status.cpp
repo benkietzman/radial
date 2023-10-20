4c3ae82670f3b4608bc33ed357242ef64c7e770c
Ben Kietzman <ben@kietzman.org>
2023-10-20 16:02:33 -0500

    Added the Status interface.

---
+++ interface/include/Status.cpp
@@ -0,0 +1,119 @@
+// -*- C++ -*-
+// Radial
+// -------------------------------------
+// file       : Status.cpp
+// author     : Ben Kietzman
+// begin      : 2023-10-20
+// copyright  : kietzman.org
+// email      : ben@kietzman.org
+/***********************************************************************
+* This program is free software; you can redistribute it and/or modify *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation; either version 2 of the License, or    *
+* (at your option) any later version.                                  *
+***********************************************************************/
+// {{{ includes
+#include "Status"
+// }}}
+extern "C++"
+{
+namespace radial
+{
+// {{{ Status()
+Status::Status(string strPrefix, int argc, char **argv, void (*pCallback)(string, const string, const bool)) : Interface(strPrefix, "status", argc, argv, pCallback)
+{
+}
+// }}}
+// {{{ ~Status()
+Status::~Status()
+{
+}
+// }}}
+// {{{ callback()
+void Status::callback(string strPrefix, const string strPacket, const bool bResponse)
+{
+  bool bResult = false;
+  string strError;
+  Json *ptJson;
+  radialPacket p;
+
+  threadIncrement();
+  strPrefix += "->Status::callback()";
+  throughput("callback");
+  unpack(strPacket, p);
+  ptJson = new Json(p.p);
+  strError = "No callback logic exists.";
+  ptJson->i("Status", ((bResult)?"okay":"error"));
+  if (!strError.empty())
+  {
+    ptJson->i("Error", strError);
+  }
+  if (bResponse)
+  {
+    ptJson->j(p.p);
+    hub(p, false);
+  }
+  delete ptJson;
+  threadDecrement();
+}
+// }}}
+// {{{ schedule()
+void Status::schedule(string strPrefix)
+{
+  string strError;
+  time_t CTime[2];
+
+  threadIncrement();
+  strPrefix += "->Status::schedule()";
+  time(&(CTime[0]));
+  while (!shutdown())
+  {
+    time(&(CTime[1]));
+    if ((CTime[1] - CTime[0]) >= 5)
+    {
+      Json *ptMessage = new Json;
+      CTime[0] = CTime[1];
+      ptMessage->i("Action", "status");
+      ptMessage->m["Nodes"] = new Json;
+      ptMessage->m["Nodes"]->m[m_strNode] = new Json;
+      m_mutexShare.lock();
+      for (auto &i : m_i)
+      {
+        stringstream ssPid;
+        ssPid << i.second->nPid;
+        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first] = new Json;
+        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("PID", ssPid, 'n');
+        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Respawn", ((i.second->bRespawn)?"1","0"), ((i.second->bRespawn)?1:0));
+        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Restricted", ((i.second->bRestricted)?"1","0"), ((i.second->bRestricted)?1:0));
+        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("AccessFunction", i.second->strAccessFunction);
+        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Command", i.second->strCommand);
+      }
+      for (auto &l : m_l)
+      {
+        if (!l->strNode.empty())
+        {
+          ptMessage->m["Nodes"]->m[l->strNode] = new Json;
+          for (auto &i : l->interfaces)
+          {
+            stringstream ssPid;
+            ssPid << i.second->nPid;
+            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first] = new Json;
+            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("PID", ssPid, 'n');
+            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Respawn", ((i.second->bRespawn)?"1","0"), ((i.second->bRespawn)?1:0));
+            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Restricted", ((i.second->bRestricted)?"1","0"), ((i.second->bRestricted)?1:0));
+            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("AccessFunction", i.second->strAccessFunction);
+            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Command", i.second->strCommand);
+          }
+        }
+      }
+      m_mutexShare.unlock();
+      live("Radial", "", ptMessage, strError);
+      delete ptMessage;
+    }
+    msleep(250);
+  }
+  threadDecrement();
+}
+// }}}
+}
+}
---
+++ interface/status.cpp
@@ -0,0 +1,35 @@
+// -*- C++ -*-
+// Radial
+// -------------------------------------
+// file       : status.cpp
+// author     : Ben Kietzman
+// begin      : 2023-10-20
+// copyright  : kietzman.org
+// email      : ben@kietzman.org
+/***********************************************************************
+* This program is free software; you can redistribute it and/or modify *
+* it under the terms of the GNU General Public License as published by *
+* the Free Software Foundation; either version 2 of the License, or    *
+* (at your option) any later version.                                  *
+***********************************************************************/
+#include "include/Status"
+using namespace radial;
+Status *gpStatus = NULL;
+void callback(string strPrefix, const string strPacket, const bool bResponse);
+int main(int argc, char *argv[])
+{
+  string strPrefix = "status->main()";
+  gpStatus = new Status(strPrefix, argc, argv, &callback);
+  thread threadSchedule(&Status::schedule, gpStatus, strPrefix);
+  pthread_setname_np(threadSchedule.native_handle(), "schedule");
+  gpStatus->process(strPrefix);
+  threadSchedule.join();
+  delete gpStatus;
+  return 0;
+}
+void callback(string strPrefix, const string strPacket, const bool bResponse)
+{
+  thread threadCallback(&Status::callback, gpStatus, strPrefix, strPacket, bResponse);
+  pthread_setname_np(threadCallback.native_handle(), "callback");
+  threadCallback.detach();
+}
