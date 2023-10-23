dde59d370050c525f4a4e0131ebcd92a120353b1
Ben Kietzman <ben@kietzman.org>
2023-10-23 15:34:20 -0500

    Saved work.

--- interface/include/Status.cpp
+++ interface/include/Status.cpp
@@ -42,7 +42,22 @@ void Status::callback(string strPrefix, const string strPacket, const bool bResp
   throughput("callback");
   unpack(strPacket, p);
   ptJson = new Json(p.p);
-  strError = "No callback logic exists.";
+  if (exist(ptJson, "Function"))
+  {
+    if (ptJson->m["Function"]->v == "status")
+    {
+      bResult = true;
+      pushStatus();
+    }
+    else
+    {
+      strError = "Please provide a valid Function:  status";
+    }
+  }
+  else
+  {
+    strError = "Please provide the Function.";
+  }
   ptJson->i("Status", ((bResult)?"okay":"error"));
   if (!strError.empty())
   {
@@ -57,10 +72,53 @@ void Status::callback(string strPrefix, const string strPacket, const bool bResp
   threadDecrement();
 }
 // }}}
+// {{{ pushStatus()
+void Status::pushStatus()
+{
+  string strError;
+  Json *ptMessage = new Json;
+
+  ptMessage->i("Action", "status");
+  ptMessage->m["Nodes"] = new Json;
+  ptMessage->m["Nodes"]->m[m_strNode] = new Json;
+  m_mutexShare.lock();
+  for (auto &i : m_i)
+  {
+    stringstream ssPid;
+    ssPid << i.second->nPid;
+    ptMessage->m["Nodes"]->m[m_strNode]->m[i.first] = new Json;
+    ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("PID", ssPid.str(), 'n');
+    ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Respawn", ((i.second->bRespawn)?"1":"0"), ((i.second->bRespawn)?1:0));
+    ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Restricted", ((i.second->bRestricted)?"1":"0"), ((i.second->bRestricted)?1:0));
+    ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("AccessFunction", i.second->strAccessFunction);
+    ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Command", i.second->strCommand);
+  }
+  for (auto &l : m_l)
+  {
+    if (!l->strNode.empty())
+    {
+      ptMessage->m["Nodes"]->m[l->strNode] = new Json;
+      for (auto &i : l->interfaces)
+      {
+        stringstream ssPid;
+        ssPid << i.second->nPid;
+        ptMessage->m["Nodes"]->m[l->strNode]->m[i.first] = new Json;
+        ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("PID", ssPid.str(), 'n');
+        ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Respawn", ((i.second->bRespawn)?"1":"0"), ((i.second->bRespawn)?1:0));
+        ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Restricted", ((i.second->bRestricted)?"1":"0"), ((i.second->bRestricted)?1:0));
+        ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("AccessFunction", i.second->strAccessFunction);
+        ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Command", i.second->strCommand);
+      }
+    }
+  }
+  m_mutexShare.unlock();
+  live("Radial", "", ptMessage, strError);
+  delete ptMessage;
+}
+// }}}
 // {{{ schedule()
 void Status::schedule(string strPrefix)
 {
-  string strError;
   time_t CTime[2];
 
   threadIncrement();
@@ -71,44 +129,8 @@ void Status::schedule(string strPrefix)
     time(&(CTime[1]));
     if ((CTime[1] - CTime[0]) >= 5)
     {
-      Json *ptMessage = new Json;
       CTime[0] = CTime[1];
-      ptMessage->i("Action", "status");
-      ptMessage->m["Nodes"] = new Json;
-      ptMessage->m["Nodes"]->m[m_strNode] = new Json;
-      m_mutexShare.lock();
-      for (auto &i : m_i)
-      {
-        stringstream ssPid;
-        ssPid << i.second->nPid;
-        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first] = new Json;
-        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("PID", ssPid.str(), 'n');
-        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Respawn", ((i.second->bRespawn)?"1":"0"), ((i.second->bRespawn)?1:0));
-        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Restricted", ((i.second->bRestricted)?"1":"0"), ((i.second->bRestricted)?1:0));
-        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("AccessFunction", i.second->strAccessFunction);
-        ptMessage->m["Nodes"]->m[m_strNode]->m[i.first]->i("Command", i.second->strCommand);
-      }
-      for (auto &l : m_l)
-      {
-        if (!l->strNode.empty())
-        {
-          ptMessage->m["Nodes"]->m[l->strNode] = new Json;
-          for (auto &i : l->interfaces)
-          {
-            stringstream ssPid;
-            ssPid << i.second->nPid;
-            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first] = new Json;
-            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("PID", ssPid.str(), 'n');
-            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Respawn", ((i.second->bRespawn)?"1":"0"), ((i.second->bRespawn)?1:0));
-            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Restricted", ((i.second->bRestricted)?"1":"0"), ((i.second->bRestricted)?1:0));
-            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("AccessFunction", i.second->strAccessFunction);
-            ptMessage->m["Nodes"]->m[l->strNode]->m[i.first]->i("Command", i.second->strCommand);
-          }
-        }
-      }
-      m_mutexShare.unlock();
-      live("Radial", "", ptMessage, strError);
-      delete ptMessage;
+      pushStatus();
     }
     msleep(250);
   }
