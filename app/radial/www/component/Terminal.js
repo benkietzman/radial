16091973c73460ad51fdbfb3f4bfc5fda632e250
Ben Kietzman <ben@kietzman.org>
2023-12-28 11:21:50 -0600

    Saved work.

--- app/radial/www/component/Terminal.js
+++ app/radial/www/component/Terminal.js
@@ -120,6 +120,7 @@ export default
       let code = window.event.keyCode;
       let key = window.event.key;
       s.message.v = null;
+      s.input.v = '';
       if (code == 13 || (code >= 37 && code <= 40)) // enter | left | up | right | down
       {
         let strFunction;
