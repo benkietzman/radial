9fad6db5b52f588a3e27b3867928f459583e22ab
Ben Kietzman <ben@kietzman.org>
2024-01-18 09:47:02 -0600

    Adjusted the socket timeouts.

--- interface/include/Terminal.cpp
+++ interface/include/Terminal.cpp
@@ -468,6 +468,7 @@ void Terminal::callback(string strPrefix, const string strPacket, const bool bRe
           {
             bool bWait = false;
             radialTerminal *t = new radialTerminal;
+            t.setSocketTimeout(100, 500);
             if (!empty(ptJson->m["Request"], "Cols"))
             {
               size_t unCols;
