48587eff14c9d0cb05264982772c277ea88e9a4e
Ben Kietzman <ben@kietzman.org>
2024-01-26 14:34:05 -0600

    Saved work.

--- app/radial/www/main.js
+++ app/radial/www/main.js
@@ -18,7 +18,7 @@ let common = new Common(
     'footer': '/include/common/js/component/footer.js',
     'menu': '/include/common/js/component/menu.js',
     'messages': '/include/common/js/component/messages.js',
-    'radialChat': '/include/common/js/component/radialChat.js'
+    'radialChat': '/include/common/js/component/radialChat.js',
     'radialStatus': '/include/common/js/component/radialStatus.js'
   },
   routes:
