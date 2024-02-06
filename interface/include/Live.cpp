1626c89498731043304ad341310423b711612c83
Ben Kietzman <ben@kietzman.org>
2024-02-06 15:07:22 -0600

    Saved work.

--- interface/include/Live.cpp
+++ interface/include/Live.cpp
@@ -230,7 +230,7 @@ void Live::callback(string strPrefix, const string strPacket, const bool bRespon
           {
             strUser[1] = ptJson->m["Request"]->m["User"]->v;
           }
-          if (!empty(ptJson->m["Request"]->m["Message"], "Action") && ptJson->m["Request"]->m["Message"]->m["Action"]->v == "chat" && strUser == "radial_bot")
+          if (!empty(ptJson->m["Request"]->m["Message"], "Action") && ptJson->m["Request"]->m["Message"]->m["Action"]->v == "chat" && strUser[1] == "radial_bot")
           {
             Json *ptIrc = new Json;
             ptIrc->i("Function", "analyze");
