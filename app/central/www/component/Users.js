f4a3f83b8d1f6bbf86a653a37b6e3d13e0830960
Ben Kietzman <ben@kietzman.org>
2024-03-07 13:04:45 -0600

    Saved work.

--- app/central/www/component/Users.js
+++ app/central/www/component/Users.js
@@ -858,7 +858,9 @@ export default
               {{#if bEdit}}
               <textarea class="form-control form-control-sm" rows="3" c-model="user.reminders.[{{@key}}].description" placeholder="Enter any reminder details..."></textarea>
               {{else}}
-              <pre style="background: inherit; color: inherit; white-space: pre-wrap;">{{description}}</pre>
+              <div class="card card-body" style="background: inherit; color: inherit;">
+                {{description}}
+              </div>
               {{/if}}
             </div>
           </div>
