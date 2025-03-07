5cc5410e701cf522d93b756417962fbb188ab6f7
Ben Kietzman <ben@kietzman.org>
2025-03-07 12:47:38 -0600

    Saved work.

--- interface/include/Central.cpp
+++ interface/include/Central.cpp
@@ -4373,7 +4373,7 @@ void Central::schedule(string strPrefix)
                                   ssMessage << char(3) << "13,06 monitor " << char(3) << " " << char(3) << "00,14 " << server.first << " " << char(3) << " " << ssAlarmsProcess.str();
                                   chat("#system", ssMessage.str(), strError);
                                   ssQuery.str("");
-                                  ssQuery << "select c.userid from application_contact a, contact_type b, person c where a.type_id = b.id and a.contact_id = c.id and b.type in ('Primary Developer', 'Backup Developer') and a.application_id = '" << esc(ptConfigProcess->m["applicationId"]->v) << "'";
+                                  ssQuery << "select c.userid from application_contact a, contact_type b, person c where a.type_id = b.id and a.contact_id = c.id and b.type in ('Primary Developer', 'Backup Developer') and a.application_id = '" << esc(ptConfigProcess->m["applicationId"]->v) << "' and a.notify = 1";
                                   auto getPerson = dbquery("central_r", ssQuery.str(), strError);
                                   if (getPerson != NULL)
                                   {
@@ -4435,7 +4435,7 @@ void Central::schedule(string strPrefix)
                 ssMessage << char(3) << "13,06 monitor " << char(3) << " " << char(3) << "00,14 " << server.first << " " << char(3) << " " << ssAlarmsSystem.str();
                 chat("#system", ssMessage.str(), strError);
                 ssQuery.str("");
-                ssQuery << "select d.userid from `server` a, server_contact b, contact_type c, person d where a.id = b.server_id and b.type_id = c.id and b.contact_id = d.id and c.type in ('Primary Admin', 'Backup Admin') and a.name = '" << esc(server.first) << "'";
+                ssQuery << "select d.userid from `server` a, server_contact b, contact_type c, person d where a.id = b.server_id and b.type_id = c.id and b.contact_id = d.id and c.type in ('Primary Admin', 'Backup Admin', 'Primary Contact') and a.name = '" << esc(server.first) << "' and b.notify = 1";
                 auto getPerson = dbquery("central_r", ssQuery.str(), strError);
                 if (getPerson != NULL)
                 {
