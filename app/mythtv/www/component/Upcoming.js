4813aec67df7cb827480c738716e507c4d62ab65
Ben Kietzman <ben@kietzman.org>
2025-04-25 14:57:44 -0500

    Saved work.

---
+++ app/mythtv/www/component/Upcoming.js
@@ -0,0 +1,125 @@
+// vim: fmr=[[[,]]]
+///////////////////////////////////////////
+// author     : Ben Kietzman
+// begin      : 2025-04-25
+// copyright  : kietzman.org
+// email      : ben@kietzman.org
+///////////////////////////////////////////
+export default
+{
+  // [[[ controller()
+  controller(id, nav)
+  {
+    // [[[ prep work
+    let a = app;
+    let c = common;
+    let s = c.scope('Upcoming',
+    {
+      // [[[ u()
+      u: () =>
+      {
+        c.update('Upcoming');
+      },
+      // ]]]
+      a: a,
+      c: c,
+      bLoaded: false
+    });
+    // ]]]
+    // [[[ getUpcoming()
+    s.getUpcoming = () =>
+    {
+      s.programs = null;
+      s.programs = [];
+      let request = {Interface: 'mythtv', 'Function': 'dvrGetUpcomingList'};
+      c.wsRequest('radial', request).then((response) =>
+      {
+        let error = {};
+        if (c.wsResponse(response, error))
+        {
+          let p = response.Response.ProgramList.Programs.Program;
+          for (let i = 0; i < p.length; i++)
+          {
+            let t = new Date(p[i].StartTime);
+            p[i].StartTime = (new Date(t - t.getTimezoneOffset() * 60 * 1000)).toISOString().split('.')[0].replace('T', ' ');
+            s.programs.push(p[i]);
+          }
+        }
+        else
+        {
+          c.pushErrorMessage(error.message);
+        }
+        s.u();
+      });
+    };
+    // ]]]
+    // [[[ init()
+    s.init = () =>
+    {
+      s.bLoaded = true;
+      if (c.isValid('MythTV'))
+      {
+        s.getUpcoming();
+      }
+      else
+      {
+        s.u();
+      }
+    };
+    // ]]]
+    // [[[ main
+    c.setMenu('Upcoming');
+    s.u();
+    if (a.ready())
+    {
+      s.init();
+    }
+    c.attachEvent('appReady', (data) =>
+    {
+      s.init();
+    });
+    // ]]]
+  },
+  // ]]]
+  // [[[ template
+  template: `
+  {{#isValid "MythTV"}}
+  <div class="table-responsive">
+    <table class="table table-condensed table-striped">
+    <thead>
+      <tr>
+        <th>Recorded</th>
+        <th>Title</th>
+        <th>SubTitle</th>
+        <th>Description</th>
+        <th>Channel</th>
+        <th>Episode</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {{#each ../programs}}
+      <tr>
+        <td>{{StartTime}}</td>
+        <td>{{Title}}</td>
+        <td>{{SubTitle}}</td>
+        <td>{{Description}}</td>
+        <td>{{Channel.ChanNum}} {{Channel.CallSign}}</td>
+        <td>S{{Season}}E{{Episode}}</td>
+        <td>{{Category}}</td>
+      </tr>
+      {{/each}}
+    </tbody>
+    </table>
+  </div>
+  {{else}}
+  {{#if ../bLoaded}}
+  {{^isValid}}
+  <p class="fw-bold text-danger">Please login to use this application.</p>
+  {{/isValid}}
+  <p class="fw-bold text-danger">You must be registered as a contact for the MythTV application in Central.</p>
+  {{/if}}
+  {{/isValid}}
+  `
+  // ]]]
+}
