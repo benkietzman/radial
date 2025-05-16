0e4bfb8a03ef4bf8a5ca3262f0ceb70021e7aecf
Ben Kietzman <ben@kietzman.org>
2025-05-16 14:56:50 -0500

    Saved work.

--- interface/include/MythTv.cpp
+++ interface/include/MythTv.cpp
@@ -187,13 +187,14 @@ chat("#radial", "exit due to length");
               }
               else if ((unPosition = strBuffers[0].find("\r\n\r\n")) != string::npos)
               {
-                string strHeader;
+                string strHeader, strTrim;
                 stringstream ssHeaders(strBuffers[0].substr(0, unPosition));
                 strBuffers[0].erase(0, (unPosition + 4));
                 while (getline(ssHeaders, strHeader))
                 {
+                  m_manip.trim(strTrim, strHeader);
+                  strHeader = strTrim;
 chat("#radial", strHeader);
-                  m_manip.trim(strHeader, strHeader);
                   if (strHeader.size() > 9 && strHeader.substr(0, 9) == "HTTP/1.1")
                   {
                     string strStatus = strHeader.substr(9, (strHeader.size() - 9));
