2a57965fda7544e3b00917ea338cf1330cd51538
Ben Kietzman <ben@kietzman.org>
2025-05-20 09:21:31 -0500

    Saved work.

--- interface/include/Interface.cpp
+++ interface/include/Interface.cpp
@@ -1328,11 +1328,8 @@ bool Interface::dataGetline(SSL *ssl, string &b, string &l, string &e)
     bRetry = false;
     if ((unPosition = b.find("\n")) != string::npos)
     {
-      string t;
       l = b.substr(0, unPosition);
       b.erase(0, (unPosition + 1));
-      t = b;
-      b = t;
     }
     else if (dataRead(ssl, b, e))
     {
