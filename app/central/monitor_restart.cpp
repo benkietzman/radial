3ac6c1d68af9854b6f2e3aa538dd91043ebc9759
Ben Kietzman <ben@kietzman.org>
2025-06-30 14:06:46 -0500

    Saved work.

---
+++ app/central/monitor_restart.cpp
@@ -0,0 +1,125 @@
+// vim600: fdm=marker
+/* -*- c++ -*- */
+///////////////////////////////////////////
+// Central Monitor
+// -------------------------------------
+// file       : monitor_restart.cpp
+// author     : Ben Kietzman
+// begin      : 2025-06-30
+// copyright  : Ben Kietzman
+// email      : ben@kietzman.org
+///////////////////////////////////////////
+// {{{ includes
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+#include <Json>
+using namespace common;
+// }}}
+// {{{ main()
+int main(int argc, char *argv[])
+{
+  if (argc == 2)
+  {
+    string strJson;
+    if (getline(cin, strJson))
+    {
+      bool bRestart = false;
+      string strCommand;
+      Json *ptJson = new Json(strJson);
+      strCommand = (string)"/usr/bin/systemctl restart" + argv[1];
+      if (ptJson->m.find("image") != ptJson->m.end() && !ptJson->m["image"]->v.empty())
+      {
+        size_t unImage;
+        stringstream ssImage(ptJson->m["image"]->v);
+        ssImage >> unImage;
+        if (ptJson->m.find("minImage") != ptJson->m.end() && !ptJson->m["minImage"]->v.empty())
+        {
+          size_t unMinImage;
+          stringstream ssMinImage(ptJson->m["minImage"]->v);
+          ssMinImage >> unMinImage;
+          if (unImage > unMinImage)
+          {
+            bRestart = true;
+          }
+        }
+        if (ptJson->m.find("maxImage") != ptJson->m.end() && !ptJson->m["maxImage"]->v.empty())
+        {
+          size_t unMaxImage;
+          stringstream ssMaxImage(ptJson->m["maxImage"]->v);
+          ssMaxImage >> unMaxImage;
+          if (unImage > unMaxImage)
+          {
+            bRestart = true;
+          }
+        }
+      }
+      if (ptJson->m.find("processes") != ptJson->m.end() && !ptJson->m["processes"]->v.empty())
+      {
+        size_t unProcesses;
+        stringstream ssProcesses(ptJson->m["processes"]->v);
+        ssProcesses >> unProcesses;
+        if (ptJson->m.find("minProcesses") != ptJson->m.end() && !ptJson->m["minProcesses"]->v.empty())
+        {
+          size_t unMinProcesses;
+          stringstream ssMinProcesses(ptJson->m["minProcesses"]->v);
+          ssMinProcesses >> unMinProcesses;
+          if (unProcesses > unMinProcesses)
+          {
+            bRestart = true;
+          }
+        }
+        if (ptJson->m.find("maxProcesses") != ptJson->m.end() && !ptJson->m["maxProcesses"]->v.empty())
+        {
+          size_t unMaxProcesses;
+          stringstream ssMaxProcesses(ptJson->m["maxProcesses"]->v);
+          ssMaxProcesses >> unMaxProcesses;
+          if (unProcesses > unMaxProcesses)
+          {
+            bRestart = true;
+          }
+        }
+      }
+      if (ptJson->m.find("resident") != ptJson->m.end() && !ptJson->m["resident"]->v.empty())
+      {
+        size_t unResident;
+        stringstream ssResident(ptJson->m["resident"]->v);
+        ssResident >> unResident;
+        if (ptJson->m.find("minResident") != ptJson->m.end() && !ptJson->m["minResident"]->v.empty())
+        {
+          size_t unMinResident;
+          stringstream ssMinResident(ptJson->m["minResident"]->v);
+          ssMinResident >> unMinResident;
+          if (unResident > unMinResident)
+          {
+            bRestart = true;
+          }
+        }
+        if (ptJson->m.find("maxResident") != ptJson->m.end() && !ptJson->m["maxResident"]->v.empty())
+        {
+          size_t unMaxResident;
+          stringstream ssMaxResident(ptJson->m["maxResident"]->v);
+          ssMaxResident >> unMaxResident;
+          if (unResident > unMaxResident)
+          {
+            bRestart = true;
+          }
+        }
+      }
+      delete ptJson;
+      if (bRestart)
+      {
+        system(strCommand.c_str());
+      }
+    }
+  }
+  else
+  {
+    cerr << "USAGE:  " << argv[0] << " [service]" << endl;
+  }
+
+  return 0;
+}
+// }}}
