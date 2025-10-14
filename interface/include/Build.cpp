3f12c4064b8baffe97cc1413690bf3fc6819c9f8
Ben Kietzman <ben@kietzman.org>
2025-10-14 15:28:47 -0500

    Saved work.

--- interface/include/Build.cpp
+++ interface/include/Build.cpp
@@ -380,7 +380,8 @@ bool Build::install(radialUser &u, string &e)
       init(u, strUser, strPassword, strPrivateKey, strSudo);
       if (sshConnect(strServer, strPort, strUser, strPassword, strPrivateKey, s, d, e))
       {
-        if (sudo(s, strSudo, d, e) && (this->*m_packages[strPackage])(s, u, d, e, true))
+        //if (sudo(s, strSudo, d, e) && (this->*m_packages[strPackage])(s, u, d, e, true))
+        if ((this->*m_packages[strPackage])(s, u, d, e, true))
         {
           b = true;
         }
