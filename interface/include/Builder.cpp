ff333e6a1ad075e5e32f496b68d62bcb74f93967
Ben Kietzman <ben@kietzman.org>
2025-10-15 16:09:47 -0500

    Saved work.

--- interface/include/Builder.cpp
+++ interface/include/Builder.cpp
@@ -663,6 +663,7 @@ bool Builder::send(string &s, const string c, list<string> &q, string &e, const 
         bRetry = true;
         if (sshSend(s, "", i, e) && !i.empty())
         {
+          unAttempts = 0;
           d.append(i);
         }
       }
