df77120358f715667fd91ca2b52d644b05ff0572
Ben Kietzman <ben@kietzman.org>
2026-05-13 10:15:12 -0500

    Saved work.

--- app/central/www/component/ApplicationsSchedule.js
+++ app/central/www/component/ApplicationsSchedule.js
@@ -137,7 +137,7 @@ export default
         </div>
       </div>
       <div class="col-md-8">
-        <div class="card border border-{{#ifCond priority "==" 4}}danger{{else ifCond ../priority "==" 3}}danger{{else ifCond ../priority "==" 2}}warning{{else}}secondary{{/ifCond}}-subtle" style="margin: 10px 0px;">
+        <div class="card border border-{{#ifCond priority "==" 4}}danger{{else}}secondary{{/ifCond}}-subtle" style="margin: 10px 0px;">
           <div class="card-header bg-{{#ifCond priority "==" 4}}danger{{else}}secondary{{/ifCond}} fw-bold text-light">
             {{#if summary}}
             {{summary}}
